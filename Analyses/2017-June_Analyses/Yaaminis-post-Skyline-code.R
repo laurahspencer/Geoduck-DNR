6b9d9ec8ffd11ff2505ed351c5160a1cfdbc5a7b
laurahspencer <lhs3@uw.edu>
2017-06-30 11:35:06 -0700

    worked through R script

--- Analyses/2017-June_Analyses/Yaaminis-post-Skyline-code.R
+++ Analyses/2017-June_Analyses/Yaaminis-post-Skyline-code.R
@@ -1,49 +1,58 @@
 ### Import Dataset ### 
-### Note: this dataset has already been massaged in Excel: sum area by protein, normalized by TIC, all n/a's removed (replaced with blanks), and columns renamed
-NormProtArea <- read.csv("2017-06-02_SKYLINE-Total-Protein-Area-NORM.csv") # import local file; working directory must be set prior to executing this line
-head(NormProtArea) 
+### Note: this dataset has already been massaged in Excel: sum area by protein, normalized by TIC, all n/a's removed (replaced with zeros), and columns renamed
+setwd("~/Documents/Roberts Lab/Geoduck-DNR/Analyses/2017-June_Analyses")
+NormProtArea <- read.csv("2017-06-02_SKYLINE-Total-Protein-Area-NORM.csv", header=TRUE, na.strings = "0") # import local file
+head(NormProtArea)
+
+### SUM TRANSITION AREA BY PROTEIN ### 
+# The "Area" values (which have already been normalized by TIC) are peak area for each transition. Sum them to determine total area for each protein
+
+NormProtAreaAgg <- aggregate(cbind(FB.E.1, CI.E.1, PG.B.1, SK.E.1, FB.B.1, WB.B.1, SK.B.1, CI.B.1, PG.E.1, WB.B.2, PG.E.2, FB.E.2, FB.B.2, CI.B.2, SK.E.2, PG.B.2, SK.B.2, CI.E.2) ~ Protein.Name, FUN = sum, data = NormProtArea, na.rm = TRUE, na.action = NULL)
+head(NormProtAreaAgg) #confirming that proteins are now summed
 
 #### AVERAGE REPLICATES #### 
 # FYI No eelgrass sample for Willapa Bay
 
-bareCaseInlet <- ave(NormProtArea$CI.B.1, NormProtArea$CI.B.2)
-head(bareCaseInlet)
-bareFidalgoBay <- ave(NormProtArea$FB.B.1, NormProtArea$FB.B.2)
-bareWillapaBay <- ave(NormProtArea$WB.B.1, NormProtArea$WB.B.2)
-bareSkokomishRiver <- ave(NormProtArea$SK.B.1, NormProtArea$SK.B.2)
-barePortGamble <- ave(NormProtArea$PG.B.1, NormProtArea$PG.B.2)
+bareCaseInlet <- ave(NormProtAreaAgg$CI.B.1, NormProtAreaAgg$CI.B.2) # create vector for each site/treatment that averages the replicates (of which there are 2)
+bareFidalgoBay <- ave(NormProtAreaAgg$FB.B.1, NormProtAreaAgg$FB.B.2)
+bareWillapaBay <- ave(NormProtAreaAgg$WB.B.1, NormProtAreaAgg$WB.B.2)
+bareSkokomishRiver <- ave(NormProtAreaAgg$SK.B.1, NormProtAreaAgg$SK.B.2)
+barePortGamble <- ave(NormProtAreaAgg$PG.B.1, NormProtAreaAgg$PG.B.2)
+eelgrassCaseInlet <- ave(NormProtAreaAgg$CI.E.1, NormProtAreaAgg$CI.E.2)
+eelgrassFidalgoBay <- ave(NormProtAreaAgg$FB.E.1, NormProtAreaAgg$FB.E.2)
+eelgrassSkokomishRiver <- ave(NormProtAreaAgg$SK.E.1, NormProtAreaAgg$SK.E.2)
+eelgrassPortGamble <- ave(NormProtAreaAgg$PG.E.1, NormProtAreaAgg$PG.E.2)
 
-eelgrassCaseInlet <- ave(NormProtArea$CI.E.1, NormProtArea$CI.E.2)
-eelgrassFidalgoBay <- ave(NormProtArea$FB.E.1, NormProtArea$FB.E.2)
-eelgrassSkokomishRiver <- ave(NormProtArea$SK.E.1, NormProtArea$SK.E.2)
-eelgrassPortGamble <- ave(NormProtArea$PG.E.1, NormProtArea$PG.E.2)
-
-NormProtAreaAveraged <- data.frame(NormProtArea$Protein.Name, bareCaseInlet, bareFidalgoBay, barePortGamble, bareSkokomishRiver, bareWillapaBay, eelgrassCaseInlet, eelgrassFidalgoBay, eelgrassPortGamble, eelgrassSkokomishRiver, eelgrassWillapaBay)
+NormProtAreaAggAveraged <- data.frame(NormProtAreaAgg$Protein.Name, bareCaseInlet, bareFidalgoBay, barePortGamble, bareSkokomishRiver, bareWillapaBay, eelgrassCaseInlet, eelgrassFidalgoBay, eelgrassPortGamble, eelgrassSkokomishRiver) # combine site/treatment vectors into new dataframe
+head(NormProtAreaAggAveraged)
 
 #### CREATE NMDS PLOT ####
-
-head(NormProtAreaAveraged) #Confirm changes
 
 #Load the source file for the biostats package
 
 source("biostats.R") #Either load the source R script or copy paste. Must run this code before NMDS.
-install.packages("vegan") #Install vegan package
 library(vegan)
 
-#Make sure first column of protein names is recognized as row names instead of values
-area.protID2 <- NormProtAreaAveraged[-1]
-rownames(area.protID2) <- NormProtAreaAveraged[,1]
+#Make sure first column of protein names is recognized as row names instead of values - this takes the first column containin protein names, and assigns it to the row "header" names 
+area.protID2 <- NormProtAreaAggAveraged[-1]
+rownames(area.protID2) <- NormProtAreaAggAveraged[,1]
+head(area.protID2)
 
 #Transpose the file so that rows and columns are switched and normalized by log(x+1)
-area2.t <- t(area.protID2[,1:10])
-area2.tra <- (area2.t+1)
-area2.tra <- data.trans(area2.tra, method = 'log', plot = FALSE)
+nrow(NormProtAreaAggAveraged)
+ncol(NormProtAreaAggAveraged)
+
+area2.t <- t(area.protID2[,1:9])
+area2.tra1 <- (area2.t+1)
+area2.tra2 <- data.trans(area2.tra1, method = 'log', plot = FALSE)
+ncol(area2.tra2)
+nrow(area2.tra2)
 
 #Make MDS dissimilarity matrix
-proc.nmds <- metaMDS(area2.tra, distance = 'bray', k = 2, trymax = 100, autotransform = FALSE)
+proc.nmds <- metaMDS(area2.t, distance = 'bray', k = 2, trymax = 100, autotransform = FALSE)
 
 #Make figure
-fig.nmds <- ordiplot(proc.nmds, choices=c(1,2), type='none', display='sites', xlab='Axis 1', ylab='Axis 2', cex=0.5)
+fig.nmds <- ordiplot(proc.nmds, choices=c(1,2), type='none', display='sites', xlab='Axis 1', ylab='Axis 2', cex=1)
 #bare=circle
 #eelgrass=triangle
 #case=red
@@ -53,7 +62,7 @@ fig.nmds <- ordiplot(proc.nmds, choices=c(1,2), type='none', display='sites', xl
 #gamble=magenta
 
 points(fig.nmds, 'sites', col=c('red', 'blue', 'black', 'green', 'magenta','red', 'blue', 'black', 'green', 'magenta'), pch=c(rep(16,5), rep(17,5)))
-legend(-0.045,0.025, pch=c(rep(16,5), 1, 2), legend=c('Case Inlet', "Fidalgo Bay", "Willapa Bay", "Skokomish", "Port Gamble", "Bare", "Eelgrass"), col=c('red', 'blue', 'black', 'green', 'magenta', 'black', 'black'))
+legend(0.185,0.1, pch=c(rep(16,5), 1, 2), legend=c('Case Inlet', "Fidalgo Bay", "Willapa Bay", "Skokomish", "Port Gamble", "Bare", "Eelgrass"), col=c('red', 'blue', 'black', 'green', 'magenta', 'black', 'black'))
 
 #### FULL HEATMAP ####
 
@@ -64,12 +73,32 @@ library(pheatmap)
 #Data should be log(x) or log(x+1) transformed for this analysis, so I'll use my area2.tra dataset.
 
 #Create heatmap
-pheatmap(area2.tra, cluster_rows = T, cluster_cols = T, clustering_distance_rows = 'euclidean', clustering_distance_cols = 'euclidean', clustering_method = 'average', show_rownames = T, show_colnames = F)
+pheatmap(area2.tra2, cluster_rows = T, cluster_cols = T, clustering_distance_rows = 'euclidean', clustering_distance_cols = 'euclidean', clustering_method = 'median', show_rownames = T, show_colnames = F)
 
 #Export preliminary heatmap as a .png
-png(filename = "fullHeatmap.png")
-pheatmap(area2.tra, cluster_rows = T, cluster_cols = T, clustering_distance_rows = 'euclidean', clustering_distance_cols = 'euclidean', clustering_method = 'average', show_rownames = T, show_colnames = F)
+png(filename = "Heatmap-by-median.png")
+pheatmap(area2.tra2, cluster_rows = T, cluster_cols = T, clustering_distance_rows = 'euclidean', clustering_distance_cols = 'euclidean', clustering_method = 'median', show_rownames = T, show_colnames = F)
 dev.off()
+
+### Find COEFFICIENT OF VARIANCES & MEDIANS ###
+
+ProtVariance <- c(var(area2.tra2[-1], area2.tra2[1], na.rm=TRUE))
+ProtMeans <- c(colMeans(area2.tra2[-1], na.rm=TRUE))
+ProtVar.Means <- data.frame(,y)
+
+ProtVar.Means <- data.frame(NormProtAreaAggAveraged[1], ProtMeans, ProtVariance)
+nrow(NormProtAreaAggAveraged[1])
+length(ProtMeans)
+
+length(ProtVariance)
+
+head(ProtMeans)
+ncol(ProtMeans)
+plot(x=ProtMeans, y=ProtVariance, main= "Protein Variance by Mean", xlab= "Mean", ylab= "CV", type="p")
+ncol(ProtMeans)
+nrow(ProtMeans)
+ncol(ProtVariance)
+nrow(ProtVariance)
 
 #### MERGE WITH GO TERMS ####
 
